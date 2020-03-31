e1ccf3508769197fa945e0bf1193595b24710d29
LenaKrina <54749924+LenaKrina@users.noreply.github.com>
2020-03-31 17:40:37 +0300

    open profile Google.com

--- first-selenium-project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
+++ first-selenium-project/src/test/java/com/telran/qa/tests/OpenGoogleTest.java
@@ -19,15 +19,27 @@ public void setUp() {
 
 @Test
 public void testOpenGoogle(){
-wd.findElement(By.className("gb_D")).click();
-wd.findElement(By.className("gb_D")).click();
+    click(By.className("gb_D"));
+    click(By.className("gb_D"));
 }
+
+    private void click(By locator) {
+        wd.findElement(locator).click();
+    }
 
     @Test
     public void testSearchGoogle() throws InterruptedException {
-        wd.findElement(By.name("q")).click();
+        click(By.name("q"));
         wd.findElement(By.name("q")).clear();
         wd.findElement(By.name("q")).sendKeys("Java");
+
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void testOpenProfile() throws InterruptedException {
+        click(By.id("gb_70"));
+
 
         Thread.sleep(3000);
     }
