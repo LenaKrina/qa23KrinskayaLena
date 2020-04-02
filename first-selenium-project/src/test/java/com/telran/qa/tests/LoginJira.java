960c684d2935e644bdf2fe8d2671c3b468090653
LenaKrina <54749924+LenaKrina@users.noreply.github.com>
2020-04-02 19:39:50 +0300

    added tests to LoginJira

--- first-selenium-project/src/test/java/com/telran/qa/tests/LoginJira.java
+++ first-selenium-project/src/test/java/com/telran/qa/tests/LoginJira.java
@@ -3,6 +3,7 @@ package com.telran.qa.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -21,16 +22,29 @@ WebDriver wd;
 
     @Test
     public void testLoginJira() throws InterruptedException {
-    wd.findElement(By.id("login-form-username")).click();
-    wd.findElement(By.id("login-form-username")).clear();
-    wd.findElement(By.id("login-form-username")).sendKeys("krinskaya");
-    wd.findElement(By.id("login-form-password")).sendKeys("3043872");
-    wd.findElement(By.id("login")).click();
+        Type(By.id("login-form-username"), "krinskaya");
+        Type(By.id("login-form-password"), "jhgjhg");
+        wd.findElement(By.id("login")).click();
 
+        Assert.assertTrue(isElementPresent(By.id("usernameerror")));
+
+        String errorMessage = wd.findElement(By.id("usernameerror")).getText();
+
+        Assert.assertEquals(errorMessage, "Sorry, your username and password are incorrect - please try again.");
 
 
         Thread.sleep(3000);
 
+    }
+
+    public void Type(By locator, String text) {
+        wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public boolean isElementPresent(By locator){
+        return wd.findElements(locator).size()>0;
     }
 
 
