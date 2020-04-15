b84bf5ba63108878e77be799f8c727592bb5e9eb
LenaKrina <54749924+LenaKrina@users.noreply.github.com>
2020-04-15 16:14:33 +0300

    new board creation

--- trello-web-tests/src/test/java/com/qa/trello/tests/BoardCreationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/BoardCreationTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -29,14 +30,22 @@ public class BoardCreationTests {
         initLogin();
         fillLoginForm("krinskaya@gmail.com", "Krina123");
         confirmLogin();
-
-        //initBoardCreation
         initBoardCreation();
-        //fillBoardForm
-        
-        //confirmBoardCreation
-        //returnToHomePage
+        fillBoardForm("new board Lena");
+        confirmBoardForm();
+        returnToHomePage();
+    }
 
+    public void returnToHomePage() {
+        click(By.cssSelector("[name='house']"));
+    }
+
+    public void confirmBoardForm() {
+        click(By.cssSelector("[type='button']"));
+    }
+
+    public void fillBoardForm(String boardName) {
+        type(By.className("_23NUW98LaZfBpQ"), boardName);
     }
 
     public void initBoardCreation() {
@@ -68,7 +77,7 @@ public class BoardCreationTests {
         wait.until(ExpectedConditions.presenceOfElementLocated(locator)).click();
     }
 
-  // @AfterMethod
+    @AfterMethod
     public void tearDown(){
         wd.quit();
     }
