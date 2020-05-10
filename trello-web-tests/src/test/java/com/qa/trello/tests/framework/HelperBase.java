e14e7516f50d756423b632d4c9c71b87528857fa
LenaKrina <54749924+LenaKrina@users.noreply.github.com>
2020-05-10 18:45:28 +0300

    added project ilCarro, registration tests

--- trello-web-tests/src/test/java/com/qa/trello/tests/framework/HelperBase.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/framework/HelperBase.java
@@ -1,9 +1,15 @@
 package com.qa.trello.tests.framework;
 
+import com.google.common.io.Files;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
 
 public class HelperBase {
     WebDriver wd;
@@ -41,5 +47,20 @@ public class HelperBase {
 
     public void returnToHomePage() {
         waitForElementClickableAndclick(By.cssSelector("[name='house']"), 20);
+    }
+
+    public void takeScreenshot(long timeMillis) {
+        File tmp = ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+        File screenshot = new File("screenshots/screen-" + timeMillis + ".png");
+
+        try {
+            Files.copy(tmp, screenshot);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void refreshPage() {
+        wd.navigate().refresh();
     }
 }
--- trello-web-tests/src/test/java/com/qa/trello/tests/tests/ProfileModificationTests.java
+++ trello-web-tests/src/test/java/com/qa/trello/tests/tests/ProfileModificationTests.java
@@ -1,18 +1,28 @@
 package com.qa.trello.tests.tests;
 
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProfileModificationTests extends TestBase {
 
     @Test
     public void testChangeProfileAvatar() throws InterruptedException {
+        long beforeScreenshot = System.currentTimeMillis();
+        app.getProfile().takeScreenshot(beforeScreenshot);
         app.getProfile().clickOnAvatar();
         app.getProfile().selectProfileAndVisibility();
         app.getProfile().initChangeProfilePhoto();
         app.getProfile().uploadPhoto();
+
+        app.getProfile().refreshPage();
+
+        long afterScreenshot = System.currentTimeMillis();
+        app.getProfile().takeScreenshot(afterScreenshot);
+
+        logger.info("screnshot before: screen-" + beforeScreenshot + ".png");
+        logger.info("screnshot after: screen-" + afterScreenshot + ".png");
+
         app.getProfile().returnToHomePage();
+
 
     }
 
